0197f28250ae406a85f0e617674416551e1d5d79
AhmeddHanyy <ahmed.abdelgawad011@eng-st.cu.edu.eg>
2022-12-22 21:33:37 +0200

    Warning

--- client_side/navbar.ts
+++ client_side/navbar.ts
@@ -49,5 +49,20 @@ warnings_notification?.addEventListener('click', function handleClick(event) {
 
 window.onload =  function () {
     logo.innerText="10";   //we should here call fetch function that will return count of warnings
+
+
+    for(let i=0;i<employee.length;i++){
+        Warnings_List.innerHTML+="<a  href=\"#\" class=\"list-group-item list-group-item-action\"><div class=\"d-flex w-100 justify-content-between\"> <h5 class=\"mb-1\">"+employee[i].emp_name+"</h5><small class=\"text-muted\">"+employee[i].emp_desg+"</small></div><p class=\"mb-1\">"+"warning description"+"</p><small class=\"text-muted\">"+employee[i].emp_id+"</small></a>"
+    }
+      
+   
   }
+
+  let Warnings_List=document.getElementById("Warninglist") as HTMLElement;
+let employee: { emp_id: number, emp_name: string, emp_desg: string }[] = [
+    { "emp_id": 0, "emp_name": "Saideep", "emp_desg": "Tech Lead" },
+    { "emp_id": 1, "emp_name": "Karthik", "emp_desg": "Manager" },
+    { "emp_id": 2, "emp_name": "Kiran", "emp_desg": "Senior Systems Engineer" }
+];//this is for testing only but will be changed with an array containing the warnings
+
 
